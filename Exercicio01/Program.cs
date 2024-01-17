207ceb26ea75a579c14f02dbdcf5459001258f2e
joaojorge <joaojorgedosanjos@gmail.com>
2024-01-17 12:04:05 -0300

    add ex02

--- Exercicio01/Program.cs
+++ Exercicio01/Program.cs
@@ -8,7 +8,7 @@
 
 var studentScore = new Dictionary<string, List<int>> {
 
-        { "João", new List<int> { 5, 5, 5, 5 } }
+        { "João", new List<int> { 5, 5, 5, 5 } },
     };
 
 List<int> scoreJoao = studentScore["João"];
@@ -21,3 +21,19 @@ foreach (var score in scoreJoao)
 
 }
 Console.WriteLine($"Média das notas {averageScoreJoao}");
+/*
+Dictionary<string, List<double>> notasAlunos = new Dictionary<string, List<double>>();
+
+// Adicione notas para alguns alunos
+notasAlunos["João"] = new List<double> { 8.5, 9.0, 7.5 };
+notasAlunos["Maria"] = new List<double> { 7.0, 8.0, 6.5 };
+
+foreach (var aluno in notasAlunos)
+{
+    double soma = 0;
+    for(int i = 0; i < aluno.Value.Count ; i++){
+        soma += aluno.Value[i];
+    }
+    double media = soma / aluno.Value.Count;
+    Console.WriteLine($"Média de {aluno.Key}: {media}");
+}*/
---
+++ Exercicio02/Program.cs
@@ -0,0 +1,18 @@
+﻿Dictionary<string, int> estoque = new Dictionary<string, int>
+{
+    { "camisetas", 50 },
+    { "calças", 30 },
+    { "tênis", 20 },
+    // Adicione mais produtos conforme necessário
+};
+
+string produto = "camisetas";
+
+if (estoque.ContainsKey(produto))
+{
+    Console.WriteLine($"Quantidade em estoque de {produto}: {estoque[produto]} unidades.");
+}
+else
+{
+    Console.WriteLine("Produto não encontrado no estoque.");
+}
